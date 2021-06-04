29080b3ec1ac8de9c37e61994e4f8991bb895601
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-06-04 14:22:28 -0300

    converting fiat to satoshi

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 
 class Converter extends Component {
   state = {
-    conversionRatio: 500
+    conversionRatio: 500,
+    fiatInput: 1
+  }
 
+  handleInput = (event) => {
+    if (event.target.value !== "") {
+      const input = event.target.value;
+
+      this.setState({
+        fiatInput: input
+      })
+    } else {
+      this.setState({
+        fiatInput: ""
+      })
+    }
   }
 
   render () {
@@ -11,10 +25,10 @@ class Converter extends Component {
       <div>
         <p>1 USD equals</p>
         <p>{this.state.conversionRatio} satoshis</p>
-        <input type="number"/>
+        <input type="number" value={this.state.fiatInput} onChange={this.handleInput}/>
         <p>fiat</p>
         <p>equals</p>
-        <p>500</p>
+        <p>{this.state.fiatInput * this.state.conversionRatio}</p>
       </div>
     )
   }
