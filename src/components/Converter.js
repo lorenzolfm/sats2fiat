322a051ccd0dd6adadd4ad224cba4077c8cf8edb
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-06-04 17:03:03 -0300

    Done with basic functionallity

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,67 +1,103 @@
 import React, { Component } from 'react';
 
-class Converter extends Component {
-  state = {
-    conversionRatio: 500,
+const IsMillionaire = (props) => {
+  if (props.satoshis >= 10e6) {
+    return <p>Congrats. You're a millionarire</p>
+  }
+  return <p>Stay humble and stack sats!</p>
+}
 
-    fiat: 1,
-    satoshi: 500
+const satsVal = 500;
+
+const toSatoshis = (fiat) => {
+  return satsVal * fiat;
+}
+
+const toFiat = (satoshis) => {
+  return satoshis / satsVal;
+}
+
+const tryConvert = (value, convert) => {
+  const input = parseFloat(value);
+  if (Number.isNaN(input)) {
+    return '';
   }
 
-  handleInput = (event) => {
-    if (event.target.id === "fiatInput") {
-      if (event.target.value !== "") {
-        const input = event.target.value;
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
 
-        this.setState({
-          fiat: input,
-          satoshi: input * this.state.conversionRatio
-        })
-      } else {
-        this.setState({
-          fiat: "",
-          satoshi: ""
-        })
-      }
-    }
+const currencyNames = {
+  f: 'fiat',
+  s: 'satoshi'
+}
 
-    if (event.target.id === "satoshiInput") {
-      if (event.target.value !== "") {
-        const input = event.target.value;
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-        this.setState({
-          fiat: input / this.state.conversionRatio,
-          satoshi: input
-        })
-      } else {
-        this.setState({
-          fiat: "",
-          satoshi: ""
-        })
-      }
-    }
+  handleChange(e) {
+    this.props.onValueChange(e.target.value);
+  }
 
+  render() {
+    const value = this.props.value;
+    const currency = this.props.currency
+
+    return (
+      <fieldset>
+        <legend>Enter value in {currencyNames[currency]}:</legend>
+        <input
+          value={value}
+          onChange={this.handleChange}
+        />
+      </fieldset>
+    )
+  }
+}
+
+class Converter extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFiatChange = this.handleFiatChange.bind(this);
+    this.handleSatoshiChange = this.handleSatoshiChange.bind(this);
+    this.state = {value: '', currency: ''};
+  }
+
+  handleFiatChange(value) {
+    this.setState({currency: 'f', value});
+  }
+
+  handleSatoshiChange(value) {
+    this.setState({currency: 's', value});
   }
 
   render () {
+    const currency = this.state.currency;
+    const value = this.state.value;
+
+    const fiat = currency === 'f' ?
+      tryConvert(value, toFiat) : value;
+    const satoshi = currency === 's' ?
+      tryConvert(value, toSatoshis) : value;
+
     return (
       <div>
-        <p>1 USD equals</p>
-        <p>{this.state.conversionRatio} satoshis</p>
-        <input
-          id="fiatInput"
-          type="number"
-          value={this.state.fiat}
-          onChange={this.handleInput}
+        <Input
+          currency="f"
+          value={fiat}
+          onValueChange={this.handleSatoshiChange}
         />
-        <p>fiat</p>
-        <p>equals</p>
-        <input
-          id="satoshiInput"
-          type="number"
-          value={this.state.satoshi}
-          onChange={this.handleInput}
+        <Input
+          currency="s"
+          value={satoshi}
+          onValueChange={this.handleFiatChange}
         />
+        <IsMillionaire
+          satoshis={parseFloat(satoshi)} />
       </div>
     )
   }
