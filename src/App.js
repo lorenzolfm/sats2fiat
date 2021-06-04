b48fb8259bf25adf4b159bca9d56527f13564cef
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-06-04 13:52:25 -0300

    inital commit for Converter component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Converter from './components/Converter'
 
 function App() {
   return (
     <div className="App">
       <h1>Sats2Fiat</h1>
+      <Converter />
     </div>
   );
 }
