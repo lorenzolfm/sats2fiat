41580ba71a9484e8514f410e1f2f63c8b46e1827
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-06-09 18:23:11 -0300

    WIP: selecting currency

--- src/components/CurrencySelector.js
+++ src/components/CurrencySelector.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 
 class CurrencySelector extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false,
+    }
+
+    this.showMenu = this.showMenu.bind(this);
+  }
+
+  showMenu (event) {
+    event.preventDefault();
+
+    this.setState({
+      showMenu: true,
+    });
+  }
+
   render() {
     return (
-      <button>BRL</button>
-    )
+      <div>
+        <button onClick={this.showMenu}>Show Menu</button>
+
+        {
+          this.state.showMenu
+            ? (
+              <div className="menu">
+                <button>USD</button>
+                <button>BRL</button>
+                <button>EUR</button>
+              </div>
+            )
+            : (
+              null
+            )
+        }
+      </div>
+    );
   }
 }
 
